a7dd865e301747518c094736019bf77e47dbd7eb
taylankalkan01 <e.taylankalkan@hotmail.com>
2021-12-31 01:09:26 -0600

    api class

---
+++ src/main/java/com/example/DemoProject/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.example.DemoProject.controller;
+
+import com.example.DemoProject.dto.UserDto;
+import com.example.DemoProject.dto.request.CreateUserRequest;
+import com.example.DemoProject.dto.request.UpdateUserRequest;
+import com.example.DemoProject.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok().body(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(userService.getUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody CreateUserRequest userRequest) {
+        return ResponseEntity.ok().body(userService.createUser(userRequest));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(
+            @PathVariable("id") Long id,
+            @RequestBody UpdateUserRequest updateUserRequest) {
+        return ResponseEntity.ok().body(userService.updateUser(id, updateUserRequest));
+    }
+
+    /*
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> deactiveUser(@PathVariable("id") Long id) {
+        userService.deactiveUser(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+
+     */
+}
+
+
